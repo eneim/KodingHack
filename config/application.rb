e6f184c2bb21ac0aa3ce2a693223238d04377dd6
tmiyamon <miyamototakuya@gmail.com>
2014-12-06 00:06:52 +0900

    configure generator

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,17 @@ module MyApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.orm :active_record
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+        fixture: true,
+        fixture_replacement: :factory_girl,
+        view_specs: false,
+        routing_specs: false,
+        helper_specs: false,
+        integration_tool: false
+    end
   end
 end
