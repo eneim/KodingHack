b80dfeaaa319384c3b1dcc22daebb1e75b94b6d2
tmiyamon <miyamototakuya@gmail.com>
2014-12-07 05:23:42 +0900

    fix donation bug and add js example

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -2,10 +2,21 @@ class DonationsController < ApplicationController
   before_action :set_donation, only: [:show, :edit, :update, :destroy]
 
   def create
-    disaster = Disaster.find_or_create params[:disaster]
-    @donation = Donation.new(user: current_user, disaster: disaster, amount: params[:amount])
+    disaster = Disaster.find_or_create_by create_params
+    @donation = Donation.new(
+      user: current_user,
+      disaster: disaster,
+      amount: params[:amount],
+      country: current_user.city.country
+    )
     @donation.save
 
-    render json: @donation
+    render json: @donation.attributes.slice('amount', 'country')
+  end
+
+  private
+
+  def create_params
+    params.require(:disaster).permit(:original_id, :name, :desc)
   end
 end
