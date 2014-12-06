889a98b29c0d1ae5858bf38216b1b1560861c495
tmiyamon <miyamototakuya@gmail.com>
2014-12-07 04:49:20 +0900

    add active style

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,20 @@
 
 $(document).ready(function(){/* off-canvas sidebar toggle */
 
-$('[data-toggle=offcanvas]').click(function() {
-  	$(this).toggleClass('visible-xs text-center');
-    $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
-    $('.row-offcanvas').toggleClass('active');
-    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
-    $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
-    $('#btnShow').toggle();
+    $('[data-toggle=offcanvas]').click(function() {
+        $(this).toggleClass('visible-xs text-center');
+        $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+        $('.row-offcanvas').toggleClass('active');
+        $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+        $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+        $('#btnShow').toggle();
+    });
 });
-});
 
 
+$('#hi > li').click(function() {
+    var $that = $(this);
+    $that.siblings().removeClass('active')
+    $that.addClass('active');
+})
 
-$(function() {
-                $('#hi li a').click( function() {
-                	var $this = $(this);
-                  $this.siblings().removeClass('active');
-				  if (!$this.hasClass('active')) {
-				      $this.addClass('active');
-				  }
-                });
-              });
