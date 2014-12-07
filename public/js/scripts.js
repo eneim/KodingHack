0a9737b478f1db67bfc2a949a350f711118c3ea1
tmiyamon <miyamototakuya@gmail.com>
2014-12-07 20:35:24 +0900

    add dirty static files

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -28,106 +28,44 @@ $('#hi > li').click(function() {
 })
 
 function getData() {
-    $.getJSON("/disasters", function(data) {
-        $.each(data, function(i, d) {
+  $.getJSON('/disasters', function(data) {
+      $.each(data, function(i, d) {
+          var item = d.raw
+          var imageURL = "";
+          if (item.dc_subject[0] == "flood") {
+              imageURL = "images/icon_flood_on.png";
+          } else if (item.dc_subject[0] == "cyclone") {
+              imageURL = "images/icon_typhoon_on.png";
+          } else if (item.dc_subject[0] == "earthquake") {
+              imageURL = "images/icon_earthquake_on.png";
+          } else if (item.dc_subject[0] == "volcanoe") {
+              imageURL = "images/icon_volcano_on.png";
+          };
 
-            var item = d.raw
-            var imageURL = "";
+          var markerColor = '#fffa77';
 
-            if (item.dc_subject[0] == "flood") {
-                imageURL = "images/icon_flood_on.png";
-            } else if (item.dc_subject[0] == "cyclone") {
-                imageURL = "images/icon_typhoon_on.png";
-            } else if (item.dc_subject[0] == "earthquake") {
-                imageURL = "images/icon_earthquake_on.png";
-            } else if (item.dc_subject[0] == "volcanoe") {
-                imageURL = "images/icon_volcano_on.png";
-            };
+          if (item.crisis_alertLevel == "Green") {
+              markerColor = '#fff601';
+          } else if (item.crisis_alertLevel == "Orange") {
+              markerColor = '#fe011f';
+          } else if (item.crisis_alertLevel == "Red") {
+              markerColor = '#d50000';
+          };
 
-            var markerColor = '#fffa77';
+          map.dataProvider.images.push({
+              latitude: item.foaf_based_near[1],
+              longitude: item.foaf_based_near[0],
+              imageURL: imageURL,
+              selectable: true,
+              width: 32,
+              height: 32,
+              title: item.dc_description,
+              xcolor: markerColor,
+              xtype: 'marker'
+          });
 
-            if (item.crisis_alertLevel == "Green") {
-                markerColor = '#fff601';
-            } else if (item.crisis_alertLevel == "Orange") {
-                markerColor = '#fe011f';
-            } else if (item.crisis_alertLevel == "Red") {
-                markerColor = '#d50000';
-            };
+      });
 
-            map.dataProvider.images.push({
-                latitude: item.foaf_based_near[1],
-                longitude: item.foaf_based_near[0],
-                imageURL: imageURL,
-                selectable: true,
-                width: 32,
-                height: 32,
-                title: item.dc_description,
-                xcolor: markerColor,
-                xtype: 'marker'
-            });
-
-        });
-
-        map.validateData();
-    });
+      map.validateData();
+  });
 };
-
-/*
-    var auth_token = 'sYmLTrRWcnh2zB3Aqkex';
-
-    function getData() {
-        page = 0;
-        console.log(page);
-
-        function loopLi() {
-            if (page <= 4) {
-                page ++;
-                var apiCall = "http://api.sigimera.org/v1/crises?page=" + page + "&auth_token=" + auth_token;
-                $.getJSON(apiCall, function(data) {
-                    $.each(data, function(i, item) {
-                        var imageURL = "";
-                        if (item.dc_subject[0] == "flood") {
-                            imageURL = "images/icon_flood_on.png";
-                        } else if (item.dc_subject[0] == "cyclone") {
-                            imageURL = "images/icon_typhoon_on.png";
-                        } else if (item.dc_subject[0] == "earthquake") {
-                            imageURL = "images/icon_earthquake_on.png";
-                        } else if (item.dc_subject[0] == "volcanoe") {
-                            imageURL = "images/icon_volcano_on.png";
-                        };
-
-                        var markerColor = '#fffa77';
-
-                        if (item.crisis_alertLevel == "Green") {
-                            markerColor = '#fff601';
-                        } else if (item.crisis_alertLevel == "Orange") {
-                            markerColor = '#fe011f';
-                        } else if (item.crisis_alertLevel == "Red") {
-                            markerColor = '#d50000';
-                        };
-
-                        map.dataProvider.images.push({
-                            latitude: item.foaf_based_near[1],
-                            longitude: item.foaf_based_near[0],
-                            imageURL: imageURL,
-                            selectable: true,
-                            width: 32,
-                            height: 32,
-                            title: item.dc_description,
-                            xcolor: markerColor,
-                            xtype: 'marker'
-                        });
-
-                    });
-
-                    map.validateData();
-                });
-
-                setTimeout(loopLi,1000);
-            }
-        }
-
-        loopLi();
-    };
-    */
-
