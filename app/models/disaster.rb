2cccd55ad1ee6bf5ad740da09a6e0faefe4ac16b
tmiyamon <miyamototakuya@gmail.com>
2014-12-07 14:29:08 +0900

    add aditional columns to disaster

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -2,4 +2,15 @@ class Disaster < ActiveRecord::Base
   has_many :donations
 
   scope :ranking_for, ->(id) { joins(:donations).where(original_id: id).group('country').order('sum_amount').sum('amount') }
+
+  def self.find_or_create_from_raw(data)
+    find_or_create_by(oritinal_id: data['crisis_eventid']) do |disaster|
+      disaster.subject     = data['dc_subject'].first
+      disaster.alert_level = data['crisis_alertLevel']
+      disaster.latitude    = data['foaf_based_near'][1]
+      disaster.longitude   = data['foaf_based_near'][0]
+      disaster.desc        = data['dc_description']
+      disaster.title       = data['dc_title']
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206170147) do
+ActiveRecord::Schema.define(version: 20141207051247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20141206170147) do
     t.string   "original_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "subject"
+    t.string   "alert_level"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "donations", force: true do |t|
